0570270788131a15416c770fd4d6b7965ae4203e
Stanislav Chvila <schvila@gmail.com>
2020-06-02 11:34:53 +0200

    Simple std sorting

---
+++ SortingStd/SortingStd.cpp
@@ -0,0 +1,27 @@
+// Sorting in C++
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+int main()
+{
+    std::vector<int> values = { 3, 5, 1, 4, 2 };
+    //std::sort(values.begin(), values.end());
+    //std::sort(values.begin(), values.end(), std::greater<int>());
+    std::sort(values.begin(), values.end(), [](int a, int b)
+    {
+        if (a == 1)
+            return false;
+        if (b == 1)
+            return true;
+
+        return a < b;
+    });
+    for (int value : values)
+        std::cout << value << std::endl;
+
+
+}
